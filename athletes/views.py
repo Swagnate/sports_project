882eccb8b73c274275204bea37a6716d3240a21b
Matveev matvei <matthew.marinara@gmail.com>
2025-01-26 23:28:34 +0300

    add templates

--- athletes/views.py
+++ athletes/views.py
@@ -49,3 +49,54 @@ def discipline_create(request):
     else:
         form = DisciplineForm()
     return render(request, 'athletes/discipline_form.html', {'form': form})
+
+def discipline_edit(request, pk):
+    discipline = get_object_or_404(Discipline, pk=pk)
+    if request.method == 'POST':
+        form = DisciplineForm(request.POST, instance=discipline)
+        if form.is_valid():
+            form.save()
+            return redirect('discipline_list')
+    else:
+        form = DisciplineForm(instance=discipline)
+    return render(request, 'athletes/discipline_form.html', {'form': form})
+
+def discipline_delete(request, pk):
+    discipline = get_object_or_404(Discipline, pk=pk)
+    if request.method == 'POST':
+        discipline.delete()
+        return redirect('discipline_list')
+    return render(request, 'athletes/discipline_confirm_delete.html', {'discipline': discipline})
+
+# Участники
+def athlete_list(request):
+    athletes = Athlete.objects.all()
+    return render(request, 'athletes/athlete_list.html', {'athletes': athletes})
+
+def athlete_create(request):
+    if request.method == 'POST':
+        form = AthleteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('athlete_list')
+    else:
+        form = AthleteForm()
+    return render(request, 'athletes/athlete_form.html', {'form': form})
+
+def athlete_edit(request, pk):
+    athlete = get_object_or_404(Athlete, pk=pk)
+    if request.method == 'POST':
+        form = AthleteForm(request.POST, instance=athlete)
+        if form.is_valid():
+            form.save()
+            return redirect('athlete_list')
+    else:
+        form = AthleteForm(instance=athlete)
+    return render(request, 'athletes/athlete_form.html', {'form': form})
+
+def athlete_delete(request, pk):
+    athlete = get_object_or_404(Athlete, pk=pk)
+    if request.method == 'POST':
+        athlete.delete()
+        return redirect('athlete_list')
+    return render(request, 'athletes/athlete_confirm_delete.html', {'athlete': athlete})
