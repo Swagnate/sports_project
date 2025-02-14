a48061664241f8e790f3ab1e73208907541ff455
Matthew Green <matthew.marinara@gmail.com>
2025-02-14 23:40:09 +0300

    redaction 1.0.0

--- sports_registration/urls.py
+++ sports_registration/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # URL для административной панели
-    path('', RedirectView.as_view(url='athletes/countries/', permanent=False)),  # Перенаправление на список стран
-    path('athletes/', include('athletes.urls')),  # Включение маршрутов приложения athletes
+    path('admin/', admin.site.urls),  
+    path('', RedirectView.as_view(url='athletes/countries/', permanent=False)),
+    path('athletes/', include('athletes.urls')),  
 ]
